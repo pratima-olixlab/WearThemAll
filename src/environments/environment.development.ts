e352aada7e232f1b7fa23ab16a43e49d1634382b
pratima-olixlab <olixlab.22@gmail.com>
2024-03-22 11:39:49 +0530

    environment file updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    firebase: {
+        projectId: 'dresswell-2ad8f',
+        appId: '1:404538998509:web:ec5ef11bc04214b2bf96e4',
+        storageBucket: 'dresswell-2ad8f.appspot.com',
+        authDomain: 'dresswell-2ad8f.firebaseapp.com',
+        messagingSenderId: '404538998509',
+        measurementId: 'G-112KGBMR2F',
+    }
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,11 @@
 export const environment = {
-    production: false,
-    firebase: {
-        apiKey: "AIzaSyCKkZV93NyZvuezikhJhtMJFsZTC63gvIY",
-        projectId: 'dresswell-2ad8f',
-        appId: '1:404538998509:web:ec5ef11bc04214b2bf96e4',
-        storageBucket: 'dresswell-2ad8f.appspot.com',
-        authDomain: 'dresswell-2ad8f.firebaseapp.com',
-        messagingSenderId: '404538998509',
-        measurementId: 'G-112KGBMR2F',
-    }
+  production: false,
+  firebase: {
+    projectId: 'dresswell-2ad8f',
+    appId: '1:404538998509:web:ec5ef11bc04214b2bf96e4',
+    storageBucket: 'dresswell-2ad8f.appspot.com',
+    authDomain: 'dresswell-2ad8f.firebaseapp.com',
+    messagingSenderId: '404538998509',
+    measurementId: 'G-112KGBMR2F',
+  },
 };
